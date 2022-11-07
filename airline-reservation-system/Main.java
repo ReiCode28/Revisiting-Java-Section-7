8d9232197d3607b0b46c9d4e31f906faceaa35e2
randy <randyr_1993@yahoo.com>
2022-11-06 21:30:51 -0500

    created Airline class

--- airline-reservation-system/Main.java
+++ airline-reservation-system/Main.java
@@ -3,59 +3,28 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
+        Person[] people = new Person[] { new Person("Cleopatra", "Egypt", "69 BC", 1),
+        new Person("Alexander the Great", "Macedon", "356 BC", 2),
+        new Person("Julius Caesar", "Rome", "100 BC", 3),
+        new Person("Hannibal", "Carthage", "247 BC", 4),
+        new Person("Confucius", "China", "551 BC", 5),
+        new Person("Pericles", "Greece", "429 BC", 6),
+        new Person("Spartacus", "Thrace", "111 BC", 7),
+        new Person("Marcus Aurelius", "Rome", "121 AD", 8),
+        new Person("Leonidas", "Greece", "540 BC", 9),
+        new Person("Sun Tzu", "China", "544 BC", 10),
+        new Person("Hammurabi", "Babylon", "1750 BC", 11),
+        };
 
-        Person person = new Person("Elon Musk", "American", "06/28/1971", 5 ); 
-        if (person.applyPassport() == true) {
-            person.setPassport();
-        
-    
+        Airline airline = new Airline();
+        for (int i = 0; i < people.length; i++) {
+            airline.setPerson(people[i]);
         }
 
-       System.out.println(person);
+        System.out.println(airline.getPerson(1));
+        System.out.println(airline.getPerson(5));
+        System.out.println(airline.getPerson(10));
+        
+
      }
-    
-  
-
-
-
-
-
-
-
-
-
-
-
-    // public static void main(String[] args) {
-    //     Person person = new Person("Elon Musk", "American", "06/28/1971", 5 );
-        
-
-    //     person.chooseSeat();
-        
-        
-    //     System.out.println(person.getName());
-    //     System.out.println(person.getNationality());
-    //     System.out.println(person.getDateOfBirth());
-    //     // System.out.println(Arrays.toString(person.passport));
-    //     System.out.println(person.getSeatNumber() + "\n");
-
-    //     if (person.applyPassport() == true){
-    //         System.out.println("Congratulations " + person.getName() + ", your passport was approved!");
-    //     } else {
-    //         System.out.println("We're sorry " + person.getName() + ". We cannot process your application.");
-    //     }
-
-    //     // Person person2 = new Person(person);
-    //     // person2.setName("Jeff Bezos");
-    //     // person2.setSeatNumber(28);
-        
-    //     // System.out.println(person2.getName());
-    //     // System.out.println(person2.getNationality());
-    //     // System.out.println(person2.getDateOfBirth());
-    //     // System.out.println(Arrays.toString(person2.passport));
-    //     // System.out.println(person2.getSeatNumber() + "\n");
- 
-    // }
-  
-  
 }
