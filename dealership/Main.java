7da9f1667e3d58eaeedc5f439efe44fb9b589cbd
randy <randyr_1993@yahoo.com>
2022-11-06 22:47:53 -0500

    added toString method to dealership class

--- dealership/Main.java
+++ dealership/Main.java
@@ -15,25 +15,5 @@ public class Main {
         for (int i = 0; i < cars.length; i++) {
             dealership.setCar(cars[i], i);
         }
-
-        dealership.sell(2);
-    
-        // nissan.setPrice(nissan.getPrice() / 2);
-        // dodge.setPrice(dodge.getPrice() / 2);
-
-        // System.out.println(Arrays.toString(nissan2.getParts()));
-        
-        
-        // nissan.drive();
-        // dodge.drive();
-        // nissan2.drive();
-        // System.out.println("This " + nissan.getMake() + " is worth $" + nissan.getPrice() +
-        //  ". It was built in " + nissan.getYear() + ". It is " + nissan.getColor() + ".\n");
-
-        // System.out.println("This " + dodge.getMake() + " is worth $" + dodge.getPrice() +
-        //  ". It was built in " + dodge.getYear() + ". It is " + dodge.getColor() + ".\n");
-
-        // System.out.println("This " + nissan2.getMake() + " is worth $" + nissan2.getPrice() +
-        //  ". It was built in " + nissan2.getYear() + ". It is " + nissan2.getColor() + ".\n");
     }
 }
