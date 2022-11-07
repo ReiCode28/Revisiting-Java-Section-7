560e10605e06a799ac9a339a0ecc92ed7c6e39fc
randy <randyr_1993@yahoo.com>
2022-11-06 21:36:08 -0500

    added sell function

--- dealership/Dealership.java
+++ dealership/Dealership.java
@@ -6,4 +6,18 @@ public class Dealership {
     public Dealership(){
         this.cars = new Car[3];
     }
+
+    //Updates one object at a time and lets the user choose which one.
+    public void setCar(Car car, int index){
+        this.cars[index] = new Car(car);
+    }
+
+    public Car getCar(int index){
+        return new Car(this.cars[index]);
+    }
+    
+    public void sell(int index){
+        this.cars[index].drive();
+        this.cars[index] = null;
+    }
 }
--- dealership/Main.java
+++ dealership/Main.java
@@ -2,21 +2,21 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        String[] parts = {"tires", "keys"};
 
-        Car nissan = new Car("Nissan", 5000, 2020, "red", parts);
-        Car dodge = new Car("Dodge", 8500, 2019, "blue", parts);
-        Car nissan2 = new Car(nissan);
-
-        nissan.setColor("Jet Black");
-        dodge.setColor("Jet Black");
-        nissan2.setParts(new String[] {"tires", "filter"}); 
+        Car[] cars = new Car[] {
+            new Car("Nissan", 5000, 2020, "red", new String[] {"tires", "keys"}),
+            new Car("Dodge", 8500, 2019, "blue", new String[] {"tires", "keys"}),
+            new Car("Nissan", 5000, 2017, "Jet Black", new String[] {"tires", "filter"}),
+            
+        };
 
         Dealership dealership = new Dealership();
 
-        System.out.println(nissan);
-        System.out.println(dodge);
-        System.out.println(nissan2);
+        for (int i = 0; i < cars.length; i++) {
+            dealership.setCar(cars[i], i);
+        }
+
+        dealership.sell(2);
     
         // nissan.setPrice(nissan.getPrice() / 2);
         // dodge.setPrice(dodge.getPrice() / 2);
