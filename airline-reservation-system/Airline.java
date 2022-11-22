0d33fcfa456373b19389527f2fc6df1ad7c30ae5
randy <randyr_1993@yahoo.com>
2022-11-22 01:33:02 -0500

    saving checkpoint

--- airline-reservation-system/Airline.java
+++ airline-reservation-system/Airline.java
@@ -1,26 +1,28 @@
 public class Airline {
-    private Person[] seats;
+    private Person[] people;
 
     public Airline(){
-        this.seats = new Person[11];
+        this.people = new Person[11];
     }
 
     public Person getPerson(int index){
-        return new Person(this.seats[index]);
+        return new Person(this.people[index]);
     }
 
     public void setPerson(Person person){
-        this.seats[person.getSeatNumber()-1] = new Person(person);
+        int index = person.getSeatNumber()-1;
+        this.people[index] = new Person(person);
     }
 
     public void createReservation(Person person){
-        while(seats[person.getSeatNumber()-1] != null){
+        
+        while(people[person.getSeatNumber()-1] != null){
             System.out.println("\n" + person.getName() + ", seat: " + person.getSeatNumber() 
             + " is already taken. Please choose another seat. \n");
             person.chooseSeat();
         }
         int index = person.getSeatNumber()-1;
-        seats[index] = new Person(person);
+        people[index] = new Person(person);
         System.out.println("Thank you " + person.getName() + " for flying with Java Airlines. Your seat number is " + person.getSeatNumber() + "\n");
     }
 }
