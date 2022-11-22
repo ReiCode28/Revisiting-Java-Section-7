35c0965d1aeda086cd5888cb164dcafc5472da50
randy <randyr_1993@yahoo.com>
2022-11-21 23:56:26 -0500

    changes to Airline.java

--- airline-reservation-system/Airline.java
+++ airline-reservation-system/Airline.java
@@ -12,4 +12,15 @@ public class Airline {
     public void setPerson(Person person){
         this.seats[person.getSeatNumber()-1] = new Person(person);
     }
+
+    public void createReservation(Person person){
+        while(seats[person.getSeatNumber()-1] != null){
+            System.out.println("\n" + person.getName() + ", seat: " + person.getSeatNumber() 
+            + " is already taken. Please choose another seat. \n");
+            person.chooseSeat();
+        }
+        int index = person.getSeatNumber()-1;
+        seats[index] = new Person(person);
+        System.out.println("Thank you " + person.getName() + " for flying with Java Airlines. Your seat number is " + person.getSeatNumber() + "\n");
+    }
 }
